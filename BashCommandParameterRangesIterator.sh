6ab5b9406859b9e59d9b23573c4a8c80b9ddc5ba
4321louis <31239298+4321louis@users.noreply.github.com>
2023-05-18 01:49:27 +1000

    Complete

---
+++ BashCommandParameterRangesIterator.sh
@@ -0,0 +1,39 @@
+#!/bin/bash 
+runOverRangesRecursive() {
+	local command=$1
+	local flag=$2
+    local length=$3
+	local paramIndex=$4
+	local accumulatedParams=$5
+
+	# base case
+	if ((length <= paramIndex)); then
+		result=$(echo "${accumulatedParams%?}")
+		printf "$command\n" "$result"
+		return 0
+	fi
+
+	
+	local range="${listOfRanges[$paramIndex]}"
+	for v in $range
+	do
+		newParams="$accumulatedParams$flag ${listOfParams[$paramIndex]}=$v"
+        runOverRangesRecursive "$command" "$flag" "$length" $((paramIndex+1)) "$newParams "
+	done
+	
+}
+
+
+runOverRanges() {
+    local command=$1
+    local flag=$2
+    declare -n listOfParamRanges=$3
+	local length="${#listOfParamRanges[@]}"
+	listOfParams=("${!listOfParamRanges[@]}")
+	listOfRanges=("${listOfParamRanges[@]}")
+	# remove weird commas
+	listOfRanges=$(echo "$listOfRanges" | sed 's/,$//;s/,//g')
+	
+	runOverRangesRecursive "$command" "$flag" "$length" 0 ""
+}
+
---
+++ examples/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+source ../BashCommandParameterRangesIterator.sh
+
+declare -A paramRanges=(
+  ["Embryonic_cas9_eff"]=$(seq 0.0 0.1 1.0),
+  ["Release_freq"]=$(seq 0.01 0.01 0.20)
+)
+
+runOverRanges "slim %s Inversion_drive.slim" "-d" paramRanges
