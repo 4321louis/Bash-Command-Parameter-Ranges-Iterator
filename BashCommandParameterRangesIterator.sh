816d79879f071a98c82188c7c3fd2c665820b7af
Louis Nowell Nicolle <lnowellnicol@spartan-login2.hpc.unimelb.edu.au>
2025-06-24 23:30:37 +1000

    Add file for which point to ignore

--- BashCommandParameterRangesIterator.sh
+++ BashCommandParameterRangesIterator.sh
@@ -5,9 +5,16 @@ runOverRangesRecursive() {
     local length=$3
 	local paramIndex=$4
 	local accumulatedParams=$5
+	local accumulatedPatternToIgnore=$6
 
 	# base case
 	if ((length <= paramIndex)); then
+		if [ -n "$minAmountForIgnore" ]; then
+			amountMatching="$(grep -c $accumulatedPatternToIgnore $fileWithDataPointsToIgnore)"
+			if [ "$amountMatching" -ge "$minAmountForIgnore" ]; then
+				return 0
+			fi
+		fi
 		result=$(echo "${accumulatedParams%?}")
 		$(printf "$command\n" "$result")
 		return 0
@@ -18,7 +25,7 @@ runOverRangesRecursive() {
 	for v in $range
 	do
 		newParams="$accumulatedParams$flag ${listOfParams[$paramIndex]}=$v"
-        runOverRangesRecursive "$command" "$flag" "$length" $((paramIndex+1)) "$newParams "
+        runOverRangesRecursive "$command" "$flag" "$length" $((paramIndex+1)) "$newParams " "${accumulatedPatternToIgnore/${listOfParams[$paramIndex]}/$v}"
 	done
 	
 }
@@ -30,13 +37,14 @@ runOverRanges() {
     declare -n listOfParamRanges=$3
 	local length="${#listOfParamRanges[@]}"
 	listOfParams=("${!listOfParamRanges[@]}")
-	listOfRanges=("${listOfParamRanges[@]}")
+	paramOrdering=$4
+	fileWithDataPointsToIgnore=$5
+	minAmountForIgnore=$6
 	# remove weird commas
 	for ((i = 0; i < length; i++)); do
-		echo $i
 		listOfRanges[$i]=$(echo "${listOfRanges[$i]}" | tr -d ',')
 	done
 	
-	runOverRangesRecursive "$command" "$flag" "$length" 0 ""
+	runOverRangesRecursive "$command" "$flag" "$length" 0 "" "$paramOrdering"
 }
 
